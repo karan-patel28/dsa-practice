4f67c4f72ee6658c280bfdb20ab89342ea36934e
karan-patel28 <karanpatel281998@gmail.com>
2026-04-30 18:01:07 -0500

    [ADD] New code added. Leetcode: 141; [UPDATE] list_operations code update: added new method to build List with cycle.

---
+++ Linked List/linked_list_cycle_141.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from list_node import ListNode
+import list_operations as lo
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+    
+if __name__ == "__main__":
+    obj = Solution()
+
+    # Example 1: cycle at index 1
+    head1 = lo.build_list([3,2,0,-4], 1)
+    print(obj.hasCycle(head1))  # Expected: True
+
+    # Example 2: cycle at index 0
+    head2 = lo.build_list([1,2], 0)
+    print(obj.hasCycle(head2))  # Expected: True
+
+    # Example 3: no cycle
+    head3 = lo.build_list([1], -1)
+    print(obj.hasCycle(head3))  # Expected: False
--- Linked List/list_operations.py
+++ Linked List/list_operations.py
@@ -1,5 +1,12 @@
 from list_node import ListNode
 
+def print_list(head):
+    curr = head
+    while curr:
+        print(curr.val, end=" -> " if curr.next else "")
+        curr = curr.next
+    print()
+
 def build_list(arr):
         dummy = ListNode(0)
         curr = dummy
@@ -8,9 +15,18 @@ def build_list(arr):
             curr = curr.next
         return dummy.next
 
-def print_list(head):
-    curr = head
-    while curr:
-        print(curr.val, end=" -> " if curr.next else "")
-        curr = curr.next
-    print()
+def build_list(arr, pos):
+        dummy = ListNode(0)
+        curr = dummy
+        cycle_node = None
+
+        for i, val in enumerate(arr):
+            curr.next = ListNode(val)
+            curr = curr.next
+            if i == pos:
+                cycle_node = curr
+
+        if pos != -1:
+            curr.next = cycle_node  # create cycle
+
+        return dummy.next
